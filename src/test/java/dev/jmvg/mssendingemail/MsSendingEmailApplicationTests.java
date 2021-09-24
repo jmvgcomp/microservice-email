b855ce7443ee8df897d7a289630bd2c0ec7cb7eb
Joanantha Matheus <jmvg.comp@gmail.com>
2021-09-24 13:24:27 -0300

    Initital config

---
+++ src/test/java/dev/jmvg/mssendingemail/MsSendingEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmvg.mssendingemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSendingEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
