4cd952148ad62bb27c7bb064ff25bf7cd983c5ca
Joanantha Matheus <jmvg.comp@gmail.com>
2021-09-25 19:27:23 -0300

    Conversor global

--- src/main/java/dev/jmvg/mssendingemail/configs/RabbitMQConfig.java
+++ src/main/java/dev/jmvg/mssendingemail/configs/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package dev.jmvg.mssendingemail.configs;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,11 @@ public class RabbitMQConfig {
     @Bean
     public Queue queue(){
         return new Queue(queue, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 
 }
